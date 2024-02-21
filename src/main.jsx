8d2bc39efdc3fd4ae6c94ae0f447bbbfe8fc42e2
duongtranngoc <duongtn0306@gmail.com>
2024-02-22 03:38:46 +0700

    Add: Integrate theme configuration from theme.js into main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
 import ReactDOM from "react-dom/client";
+
 import App from "./App.jsx";
+import theme from "./theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <CssVarsProvider theme={theme} defaultMode="system">
+    <CssBaseline />
     <App />
-  </React.StrictMode>
+  </CssVarsProvider>
 );
---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { experimental_extendTheme as extendTheme } from "@mui/material/styles";
+
+const theme = extendTheme({
+  taskBoardCustom: {},
+
+  colorSchemes: {
+    light: {},
+
+    dark: {},
+  },
+
+  components: {},
+});
+
+export default theme;
